6a9a1a4d674f7007c18285582c8eec60a3899086
gongjuyeonhee <yyeonhee912@naver.com>
2024-08-14 15:30:28 +0900

    next() function

--- server/routes/user.js
+++ server/routes/user.js
@@ -3,6 +3,18 @@ import { asyncHandler } from "../utils/asyncErrorHandler.js";
 
 const userRouter = express.Router();
 
+function testNext(req, res, next) {
+  console.log("next 사용해보기");
+  next();
+}
+
+function greeting(req, res, next) {
+  console.log("첨 사용");
+  res.json({ message: "첨 사용해봄" });
+}
+
+userRouter.get("/hello", testNext, greeting);
+
 userRouter.get("/", (req, res) => {
   res.send({ message: "user router" });
 });
