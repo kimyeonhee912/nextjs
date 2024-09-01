8a909327d4aac065d7ecc96c388bcd7346391794
gongjuyeonhee <yyeonhee912@naver.com>
2024-09-02 00:45:38 +0900

    feat: Successfully inserted data into MySQL

--- server/server.js
+++ server/server.js
@@ -3,11 +3,11 @@
 import express from "express";
 import next from "next";
 import mongoose from "mongoose"; //mongoose 연결
-import mysql from "mysql2/promise"; //mysql 연결
+
 import dotenv from "dotenv";
 import TaskRouter from "./routes/task.js";
 import userRouter from "./routes/user.js";
-import TestRouter from "./routes/testpost.js";
+import testRouter from "./routes/testpost.js";
 
 dotenv.config({ path: ".env.local" });
 const dev = true;
@@ -18,19 +18,6 @@ mongoose
   .connect(process.env.NEXT_PUBLIC_DATABASE_URL)
   .then(() => console.log("Connected to DB"));
 
-// MySQL 연결 설정
-const mysqlConnection = await mysql.createConnection({
-  host: process.env.NEXT_PUBLIC_MYSQL_HOST,
-  user: process.env.NEXT_PUBLIC_MYSQL_USER,
-  password: process.env.NEXT_PUBLIC_MYSQL_PASSWORD,
-  database: process.env.NEXT_PUBLIC_MYSQL_DATABASE,
-});
-
-mysqlConnection
-  .connect()
-  .then(() => console.log("Connected to MySQL"))
-  .catch((err) => console.error("MySQL connection error:", err));
-
 app.prepare().then(() => {
   const server = express();
   server.use(express.json());
@@ -38,7 +25,7 @@ app.prepare().then(() => {
   // task 라우터 등록
   server.use("/api/task", TaskRouter);
   server.use("/api/user", userRouter);
-  server.use("/api/test", TestRouter);
+  server.use("/api/test", testRouter);
 
   // Next.js 페이지 요청 처리
   server.all("*", (req, res) => {
